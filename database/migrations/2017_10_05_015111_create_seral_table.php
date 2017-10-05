20df90bf84319e6c1c9761f4ef7d814da125853c
Eugene <eako0508@gmail.com>
2017-10-04 21:53:22 -0400

    make:migration create_serial_table

--- database/migrations/2017_10_05_015111_create_seral_table.php
+++ database/migrations/2017_10_05_015111_create_seral_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateSeralTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sn_tables', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+			$table->string('company');
+			$table->integer('invoice');
+			$table->string('product');
+			$table->string('product_sn');
+			$table->string('hdd');
+			$table->string('hdd_sn');
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sn_tables');
     }
 }
